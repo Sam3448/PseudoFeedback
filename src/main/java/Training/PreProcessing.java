565a53707228a12fb1cea91fa23901e733e4447e
weicheng zhang <zwc347@gmail.com>
2018-02-14 14:40:58 -0500

    jdk update and error bug fixed

--- src/main/java/Training/PreProcessing.java
+++ src/main/java/Training/PreProcessing.java
@@ -2,6 +2,8 @@ package Training;
 /**
  * Created by SamZhang on 2/14/18.
 **/
+import org.datavec.api.util.ClassPathResource;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,13 +13,16 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class PreProcessing {
-    static String inputFile = "/Users/SamZhang/Documents/RA2017/Pseudo/src/bitext/MATERIAL_BASE-1A-BUILD_bitext.txt";
+    static File inputFile;
     static String swOutputFile = "/Users/SamZhang/Documents/RA2017/Pseudo/src/bitext/en.txt";
     static String enOutputFile = "/Users/SamZhang/Documents/RA2017/Pseudo/src/bitext/sw.txt";
     static HashMap<String, String> sw = new HashMap(), en = new HashMap();
+    static void init() throws IOException{
+        inputFile = new ClassPathResource("/MTDoc/MATERIAL_BASE-1A-BUILD_bitext.txt").getFile();
+    }
     public static void processing() throws IOException{
-        File input = new File(inputFile);
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(input)));
+        init();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile)));
         String line = "";
         while((line = br.readLine()) != null){
             String[] cur = line.split("\t");
--- src/main/java/Training/WordRepresentation.java
+++ src/main/java/Training/WordRepresentation.java
@@ -2,12 +2,22 @@ package Training;
 
 import java.io.IOException;
 
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class WordRepresentation {
     public static void main(String[] args) throws IOException{
         // TODO Auto-generated method stub
         PreProcessing.processing();
         PreProcessing.fileOutput();
-        System.out.print("here");
     }
 }
 
